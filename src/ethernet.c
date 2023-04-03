4ad84717fcf0b54e0586ded9fa0b55f1aaa3eac1
kjm <jimmyke868@gmail.com>
2023-04-02 22:37:13 -0700

    Done: lab2

--- src/ethernet.c
+++ src/ethernet.c
@@ -11,6 +11,20 @@
 void ethernet_in(buf_t *buf)
 {
     // TO-DO
+    if (buf->len < sizeof(ether_hdr_t))
+    {
+        return;
+    }
+    ether_hdr_t *hdr = (ether_hdr_t *) buf->data;
+    if (buf_remove_header(buf, sizeof(ether_hdr_t)) < 0) 
+    {
+        fprintf(stderr, "ethernet_in: buf_remove_header");
+        return;
+    }
+    if (net_in(buf, swap16(hdr->protocol16), hdr->src) < 0)
+    {
+        fprintf(stderr, "ethernet_in: net_in");
+    }
 }
 /**
  * @brief 处理一个要发送的数据包
@@ -22,6 +36,21 @@ void ethernet_in(buf_t *buf)
 void ethernet_out(buf_t *buf, const uint8_t *mac, net_protocol_t protocol)
 {
     // TO-DO
+    if (buf->len < ETHERNET_MIN_TRANSPORT_UNIT && buf_add_padding(buf, 46 - buf->len) < 0) 
+    {
+        fprintf(stderr, "ethernet_out: buf_add_padding");
+        return;
+    }
+
+    buf_add_header(buf, sizeof(ether_hdr_t));
+    ether_hdr_t *hdr = (ether_hdr_t *) buf->data;
+    memcpy(hdr->dst, mac, NET_MAC_LEN);
+    memcpy(hdr->src, net_if_mac, NET_MAC_LEN);
+    hdr->protocol16 = swap16(protocol);
+    if (driver_send(buf) < 0) 
+    {
+        fprintf(stderr, "ethernet: driver_send");
+    }
 }
 /**
  * @brief 初始化以太网协议
